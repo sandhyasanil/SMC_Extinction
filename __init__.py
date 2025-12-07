5840981a76d20382b742f99aed85b0d954a76fa7
Sandhya Sanil <sandhyasanil@gmail.com>
2025-12-07 23:07:39 +0530

    Updating example code

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .model import ExtinctionCurve
+__all__ = ['ExtinctionCurve']
--- examples/sample.py
+++ examples/sample.py
@@ -1,1 +1,35 @@
+"""
+Example usage of the ExtinctionCurve model.
+"""
+# Append parent directory to sys.path for imports
+import sys
+import os
+
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from model import ExtinctionCurve
+
+
+# Example wavelength grid (inverse micron)
+x = np.linspace(1.0, 8.0, 500)
+
+# Instantiate the model
+curve = ExtinctionCurve(x, rv=3.1)
+
+
+# Compute extinction curve
+k = curve.evaluate()
+
+# Plot
+plt.figure()
+plt.plot(x, k)
+plt.xlabel(r"1/$\lambda$ ($\mu$m$^{-1}$)")
+plt.ylabel(r"A($\lambda$)/A(V)")
+plt.title("Example Extinction Curve")
+plt.grid()
+plt.show()
 
